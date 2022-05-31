dcd601f44ebfd17d4ae1e1c064053c464c008be7
Rui Fernandes <ruipedro16@protonmail.com>
2022-05-31 01:00:58 +0100

    report & query updates

--- report/compile.sh
+++ report/compile.sh
@@ -2,11 +2,13 @@
 
 # Install latex with sudo apt-get install -y texlive-full
 
+cp ../plots/* img
+
 if [ "$1" = "-s" ]; then
-  pdflatex report.tex > /dev/null 2>&1
-  pdflatex report.tex > /dev/null 2>&1
+  pdflatex -shell-escape report.tex > /dev/null 2>&1
+  pdflatex -shell-escape report.tex > /dev/null 2>&1
   rm -f report.aux report.lof report.log report.out report.toc texput.log
 else
-  pdflatex report.tex
-  pdflatex report.tex
+  pdflatex -shell-escape report.tex
+  pdflatex -shell-escape report.tex
 fi
