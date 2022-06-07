3bceed4794ca2a5e7112f411de8cbe3f97649a83
Rui Fernandes <ruipedro16@protonmail.com>
2022-06-07 16:51:55 +0100

    inicio dos slides

---
+++ slides/compile.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Install latex with sudo apt-get install -y texlive-full
+
+mkdir -p img
+cp ../plots/* img
+
+pdflatex -shell-escape slides.tex
+
+rm -f slides.aux slides.fdb_latexmk slides.fls slides.log slides.nav slides.out slides.snm slides.toc 
+rm -f slides.synctex.gz texput.log
