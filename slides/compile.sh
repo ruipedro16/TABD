fe12d6f301743e90c1de9603f9ed7f040ae173b1
Rui Fernandes <ruipedro16@protonmail.com>
2022-06-08 10:58:57 +0100

    slides done

--- slides/compile.sh
+++ slides/compile.sh
@@ -2,10 +2,6 @@
 
 # Install latex with sudo apt-get install -y texlive-full
 
-mkdir -p img
-cp ../plots/* img
-cp ../report/img/datawarehouse.pdf img
-
 pdflatex -shell-escape slides.tex
 
 rm -f slides.aux slides.fdb_latexmk slides.fls slides.log slides.nav slides.out slides.snm slides.toc 
